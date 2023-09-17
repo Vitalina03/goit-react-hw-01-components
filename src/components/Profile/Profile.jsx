009b9c964cd1b81f80e5925f357c07c3e6ba61a9
Vitalina03 <127879557+Vitalina03@users.noreply.github.com>
2023-09-17 11:02:15 +0300

    2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,35 +1,35 @@
-
 import css from './Profile.module.css';
 
-export const Profile = ({username,tag,location,avatar,stats:{followers,views,likes}}) => {
-    return (<div className ={css.profile}>
-    <div className={css.description}>
-      <img
-        src={avatar}
-        className={css.avatar}
-      />
-      <p className={css.name}>{username}</p>
-      <p className={css.tag}>{'@'+tag}</p>
-      <p className={css.location}>{location}</p>
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{'@' + tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>1000</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>2000</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>3000</span>
+        </li>
+      </ul>
     </div>
-  
-    <ul className={css.stats}>
-      <li>
-        <span className={css.label}>Followers</span>
-        <span className={css.quantity}>1000</span>
-      </li>
-      <li>
-    <span className={css.label}>Views</span>
-        <span className={css.quantity}>2000</span>
-      </li>
-      <li>
-        <span className={css.label}>Likes</span>
-        <span className={css.quantity}>3000</span>
-      </li>
-    </ul>
-  </div>
-  
   );
 };
-
-
